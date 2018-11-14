ce0edfafa03a2bce61778d6aa9ba31465e3c5203
Vadim Balakhanov <baldis@bk.ru>
2018-11-14 09:42:47 +0300

    I18n updated
    Signed-off-by: Vadim Balakhanov <baldis@bk.ru>

--- lang_en_US.ts
+++ lang_en_US.ts
@@ -4,77 +4,77 @@
 <context>
     <name>AppState</name>
     <message>
-        <location filename="appstate.cpp" line="118"/>
+        <location filename="appstate.cpp" line="111"/>
         <source>No words to learn, get another dictionary</source>
         <translation>No words to learn, get another dictionary</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="121"/>
-        <location filename="appstate.cpp" line="197"/>
+        <location filename="appstate.cpp" line="114"/>
+        <location filename="appstate.cpp" line="190"/>
         <source>You gonna learn %1 new words</source>
         <translation>You gonna learn %1 new words</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="135"/>
+        <location filename="appstate.cpp" line="128"/>
         <source>No words to repeat, learn something first</source>
         <translation>No words to repeat, learn something first</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="138"/>
-        <location filename="appstate.cpp" line="194"/>
+        <location filename="appstate.cpp" line="131"/>
+        <location filename="appstate.cpp" line="187"/>
         <source>You gonna repeat %1 words</source>
         <translation>You will repeat %1 words</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="146"/>
+        <location filename="appstate.cpp" line="139"/>
         <source>No words to remind, train first</source>
         <translation>No words to repeat, train first</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="149"/>
-        <location filename="appstate.cpp" line="191"/>
+        <location filename="appstate.cpp" line="142"/>
+        <location filename="appstate.cpp" line="184"/>
         <source>You gonna remind %1 words</source>
         <translation>You gonna repeat %1 words</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="163"/>
+        <location filename="appstate.cpp" line="156"/>
         <source>Run took %1 seconds.
 Try to improve?</source>
         <translation>Run took %1 seconds.
 Try to improve?</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="167"/>
+        <location filename="appstate.cpp" line="160"/>
         <source>Run took %2 seconds (previous %1).
 Try to improve?</source>
         <translation>Run took %2 seconds (previous %1).
 Try to improve?</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="242"/>
-        <location filename="appstate.cpp" line="245"/>
+        <location filename="appstate.cpp" line="235"/>
+        <location filename="appstate.cpp" line="238"/>
         <source>Let&apos;s check these words</source>
         <translation>Let&apos;s check these words</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="252"/>
+        <location filename="appstate.cpp" line="245"/>
         <source>You should fix that error</source>
         <translation>You should fix that error</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="256"/>
-        <location filename="appstate.cpp" line="259"/>
+        <location filename="appstate.cpp" line="249"/>
+        <location filename="appstate.cpp" line="252"/>
         <source>Some statistics here</source>
         <translation>Some statistics here</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="264"/>
-        <location filename="appstate.cpp" line="267"/>
+        <location filename="appstate.cpp" line="257"/>
+        <location filename="appstate.cpp" line="260"/>
         <source>You should fix %1 errors</source>
         <translation>You should fix %1 errors</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="278"/>
+        <location filename="appstate.cpp" line="271"/>
         <source>Let&apos;s try to improve</source>
         <translation>Let&apos;s try to improve</translation>
     </message>
@@ -82,29 +82,41 @@ Try to improve?</translation>
 <context>
     <name>Menu</name>
     <message>
-        <location filename="Menu.qml" line="105"/>
         <source>Words in serie</source>
-        <translation>Words in serie</translation>
+        <translation type="vanished">Words in serie</translation>
     </message>
     <message>
-        <location filename="Menu.qml" line="123"/>
+        <location filename="Menu.qml" line="34"/>
         <source>Load file</source>
         <translation>Load CSV file</translation>
     </message>
     <message>
-        <location filename="Menu.qml" line="132"/>
+        <location filename="Menu.qml" line="43"/>
         <source>Load stolen</source>
         <translation>Load JS file</translation>
     </message>
     <message>
-        <location filename="Menu.qml" line="141"/>
+        <location filename="Menu.qml" line="52"/>
         <source>Dictionaries</source>
         <translation>Dictionaries</translation>
     </message>
     <message>
-        <location filename="Menu.qml" line="147"/>
+        <location filename="Menu.qml" line="58"/>
         <source>Settings</source>
         <translation>Settings</translation>
+    </message>
+    <message>
+        <location filename="Menu.qml" line="64"/>
+        <source>Words</source>
+        <translation>Words</translation>
+    </message>
+</context>
+<context>
+    <name>SettingDialog</name>
+    <message>
+        <location filename="SettingDialog.qml" line="11"/>
+        <source>Words in serie</source>
+        <translation>Words in serie</translation>
     </message>
 </context>
 <context>
@@ -115,43 +127,43 @@ Try to improve?</translation>
         <translation>Word learn</translation>
     </message>
     <message>
-        <location filename="main.qml" line="71"/>
-        <location filename="main.qml" line="101"/>
+        <location filename="main.qml" line="75"/>
+        <location filename="main.qml" line="105"/>
         <source>AGAIN?</source>
         <translation>AGAIN</translation>
     </message>
     <message>
-        <location filename="main.qml" line="77"/>
+        <location filename="main.qml" line="81"/>
         <source>LEARN</source>
         <translation>LEARN</translation>
     </message>
     <message>
-        <location filename="main.qml" line="81"/>
+        <location filename="main.qml" line="85"/>
         <source>CHECK</source>
         <translation>CHECK</translation>
     </message>
     <message>
-        <location filename="main.qml" line="85"/>
+        <location filename="main.qml" line="89"/>
         <source>ERRORS</source>
         <translation>ERRORS</translation>
     </message>
     <message>
-        <location filename="main.qml" line="89"/>
+        <location filename="main.qml" line="93"/>
         <source>TRAIN</source>
         <translation>TRAIN</translation>
     </message>
     <message>
-        <location filename="main.qml" line="93"/>
+        <location filename="main.qml" line="97"/>
         <source>REPEAT</source>
         <translation>REPEAT</translation>
     </message>
     <message>
-        <location filename="main.qml" line="97"/>
+        <location filename="main.qml" line="101"/>
         <source>COMPLETE</source>
         <translation>COMPLETE</translation>
     </message>
     <message>
-        <location filename="main.qml" line="105"/>
+        <location filename="main.qml" line="109"/>
         <source>UNEXPECTED! </source>
         <translation>UNEXPECTED! </translation>
     </message>
--- lang_ru_RU.ts
+++ lang_ru_RU.ts
@@ -4,77 +4,77 @@
 <context>
     <name>AppState</name>
     <message>
-        <location filename="appstate.cpp" line="118"/>
+        <location filename="appstate.cpp" line="111"/>
         <source>No words to learn, get another dictionary</source>
         <translation>Нет новых слов, переключите словарь</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="121"/>
-        <location filename="appstate.cpp" line="197"/>
+        <location filename="appstate.cpp" line="114"/>
+        <location filename="appstate.cpp" line="190"/>
         <source>You gonna learn %1 new words</source>
         <translation>%1 новых слов готовы к изучению</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="135"/>
+        <location filename="appstate.cpp" line="128"/>
         <source>No words to repeat, learn something first</source>
         <translation>Нет слов для закрепления, выучите новые слова</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="138"/>
-        <location filename="appstate.cpp" line="194"/>
+        <location filename="appstate.cpp" line="131"/>
+        <location filename="appstate.cpp" line="187"/>
         <source>You gonna repeat %1 words</source>
         <translation>%1 новых слов готовы к закреплению</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="146"/>
+        <location filename="appstate.cpp" line="139"/>
         <source>No words to remind, train first</source>
         <translation>Нет слов для повторения, выучите новые слова</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="149"/>
-        <location filename="appstate.cpp" line="191"/>
+        <location filename="appstate.cpp" line="142"/>
+        <location filename="appstate.cpp" line="184"/>
         <source>You gonna remind %1 words</source>
         <translation>%1 новых слов готовы к повторению</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="163"/>
+        <location filename="appstate.cpp" line="156"/>
         <source>Run took %1 seconds.
 Try to improve?</source>
         <translation>Повтор занял %1 секунд.
 Попробуете улучшить результат?</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="167"/>
+        <location filename="appstate.cpp" line="160"/>
         <source>Run took %2 seconds (previous %1).
 Try to improve?</source>
         <translation>Повтор занял %1 секунд (предыдущий %2 секунд).
 Попробуете улучшить результат?</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="242"/>
-        <location filename="appstate.cpp" line="245"/>
+        <location filename="appstate.cpp" line="235"/>
+        <location filename="appstate.cpp" line="238"/>
         <source>Let&apos;s check these words</source>
         <translation>Проверьте выученные слова</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="252"/>
+        <location filename="appstate.cpp" line="245"/>
         <source>You should fix that error</source>
         <translation>Нужно исправить ошибки</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="256"/>
-        <location filename="appstate.cpp" line="259"/>
+        <location filename="appstate.cpp" line="249"/>
+        <location filename="appstate.cpp" line="252"/>
         <source>Some statistics here</source>
         <translation>Здесь что-нибудь будет</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="264"/>
-        <location filename="appstate.cpp" line="267"/>
+        <location filename="appstate.cpp" line="257"/>
+        <location filename="appstate.cpp" line="260"/>
         <source>You should fix %1 errors</source>
         <translation>Нужно исправить %1 ошибок</translation>
     </message>
     <message>
-        <location filename="appstate.cpp" line="278"/>
+        <location filename="appstate.cpp" line="271"/>
         <source>Let&apos;s try to improve</source>
         <translation>Попробуем улучшить результат</translation>
     </message>
@@ -82,29 +82,41 @@ Try to improve?</source>
 <context>
     <name>Menu</name>
     <message>
-        <location filename="Menu.qml" line="105"/>
         <source>Words in serie</source>
-        <translation>Слов в серии для изучения</translation>
+        <translation type="vanished">Слов в серии для изучения</translation>
     </message>
     <message>
-        <location filename="Menu.qml" line="123"/>
+        <location filename="Menu.qml" line="34"/>
         <source>Load file</source>
         <translation>Загрузить таблицу</translation>
     </message>
     <message>
-        <location filename="Menu.qml" line="132"/>
+        <location filename="Menu.qml" line="43"/>
         <source>Load stolen</source>
         <translation>Загрузить словарь</translation>
     </message>
     <message>
-        <location filename="Menu.qml" line="141"/>
+        <location filename="Menu.qml" line="52"/>
         <source>Dictionaries</source>
         <translation>Словари</translation>
     </message>
     <message>
-        <location filename="Menu.qml" line="147"/>
+        <location filename="Menu.qml" line="58"/>
         <source>Settings</source>
         <translation>Настройки</translation>
+    </message>
+    <message>
+        <location filename="Menu.qml" line="64"/>
+        <source>Words</source>
+        <translation>Редактирование слов</translation>
+    </message>
+</context>
+<context>
+    <name>SettingDialog</name>
+    <message>
+        <location filename="SettingDialog.qml" line="11"/>
+        <source>Words in serie</source>
+        <translation>Слов в серии для изучения</translation>
     </message>
 </context>
 <context>
@@ -115,43 +127,43 @@ Try to improve?</source>
         <translation>Изучение слов</translation>
     </message>
     <message>
-        <location filename="main.qml" line="71"/>
-        <location filename="main.qml" line="101"/>
+        <location filename="main.qml" line="75"/>
+        <location filename="main.qml" line="105"/>
         <source>AGAIN?</source>
         <translation>Повтор</translation>
     </message>
     <message>
-        <location filename="main.qml" line="77"/>
+        <location filename="main.qml" line="81"/>
         <source>LEARN</source>
         <translation>Изучение</translation>
     </message>
     <message>
-        <location filename="main.qml" line="81"/>
+        <location filename="main.qml" line="85"/>
         <source>CHECK</source>
         <translation>Проверка</translation>
     </message>
     <message>
-        <location filename="main.qml" line="85"/>
+        <location filename="main.qml" line="89"/>
         <source>ERRORS</source>
         <translation>Работа над ошибками</translation>
     </message>
     <message>
-        <location filename="main.qml" line="89"/>
+        <location filename="main.qml" line="93"/>
         <source>TRAIN</source>
         <translation>Закрепление</translation>
     </message>
     <message>
-        <location filename="main.qml" line="93"/>
+        <location filename="main.qml" line="97"/>
         <source>REPEAT</source>
         <translation>Повторение</translation>
     </message>
     <message>
-        <location filename="main.qml" line="97"/>
+        <location filename="main.qml" line="101"/>
         <source>COMPLETE</source>
         <translation>Поздравляем</translation>
     </message>
     <message>
-        <location filename="main.qml" line="105"/>
+        <location filename="main.qml" line="109"/>
         <source>UNEXPECTED! </source>
         <translation>БАГ! </translation>
     </message>
